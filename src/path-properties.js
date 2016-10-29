0e151df44870eb7120ea158885f23a279eb25036
Roger Veciana i Rovira <rveciana@gmail.com>
2016-10-29 21:40:15 +0200

    First getPointAt version

--- src/path-properties.js
+++ src/path-properties.js
@@ -4,7 +4,7 @@ import Bezier from "bezier-js";
 export default function(svgString) {
   var length = 0;
   var partial_lengths = [];
-
+  var functions = [];
 
   function svgProperties(string){
     var parsed = parse(string);
@@ -15,42 +15,54 @@ export default function(svgString) {
       //moveTo
       if(parsed[i][0] === "M"){
         cur = [parsed[i][1], parsed[i][2]];
+        functions.push(null);
       } else if(parsed[i][0] === "m"){
         cur = [parsed[i][1] + cur[0], parsed[i][2] + cur[1]];
+        functions.push(null);
       }
       //lineTo
       else if(parsed[i][0] === "L"){
         length = length + Math.sqrt(Math.pow(cur[0] - parsed[i][1], 2) + Math.pow(cur[1] - parsed[i][2], 2));
+        functions.push(linearPosition(cur[0], parsed[i][1], cur[1], parsed[i][2]));
         cur = [parsed[i][1], parsed[i][2]];
+
       } else if(parsed[i][0] === "l"){
         length = length + Math.sqrt(Math.pow(parsed[i][1], 2) + Math.pow(parsed[i][2], 2));
         cur = [parsed[i][1] + cur[0], parsed[i][2] + cur[1]];
+        functions.push();
       } else if(parsed[i][0] === "H"){
         length = length + Math.abs(cur[0] - parsed[i][1]);
         cur[0] = parsed[i][1];
+        functions.push();
       } else if(parsed[i][0] === "h"){
         length = length + Math.abs(parsed[i][1]);
         cur[0] = parsed[i][1] + cur[0];
+        functions.push();
       } else if(parsed[i][0] === "V"){
         length = length + Math.abs(cur[1] - parsed[i][1]);
         cur[1] = parsed[i][1];
+        functions.push();
       } else if(parsed[i][0] === "v"){
         length = length + Math.abs(parsed[i][1]);
         cur[1] = parsed[i][1] + cur[1];
+        functions.push();
       //Close path
       }  else if(parsed[i][0] === "z" || parsed[i][0] === "Z"){
         length = length + Math.sqrt(Math.pow(parsed[0][1] - cur[0], 2) + Math.pow(parsed[0][1] - cur[1], 2));
         cur = [parsed[0][1], parsed[0][2]];
+        functions.push();
       }
       //Cubic Bezier curves
       else if(parsed[i][0] === "C"){
         curve = new Bezier(cur[0], cur[1] , parsed[i][1], parsed[i][2] , parsed[i][3], parsed[i][4] , parsed[i][5], parsed[i][6]);
         length = length + curve.length();
         cur = [parsed[i][5], parsed[i][6]];
+        functions.push(curve);
       } else if(parsed[i][0] === "c"){
         curve = new Bezier(cur[0], cur[1] , cur[0] + parsed[i][1], cur[1] + parsed[i][2] , cur[0] + parsed[i][3], cur[1] + parsed[i][4] , cur[0] + parsed[i][5], cur[1] + parsed[i][6]);
         length = length + curve.length();
         cur = [parsed[i][5] + cur[0], parsed[i][6] + cur[1]];
+        functions.push(curve);
       } else if(parsed[i][0] === "S"){
         if(i>0 && ["C","c","S","s"].indexOf(parsed[i-1][0]) > -1){
           curve = new Bezier(cur[0], cur[1] , cur[0] + parsed[i][1] - parsed[i][3], cur[1] + parsed[i][2] - parsed[i][4], parsed[i][1], parsed[i][2] , parsed[i][3], parsed[i][4]);
@@ -59,6 +71,7 @@ export default function(svgString) {
         }
         length = length + curve.length();
         cur = [parsed[i][3], parsed[i][4]];
+        functions.push(curve);
       }  else if(parsed[i][0] === "s"){
         if(i>0 && ["C","c","S","s"].indexOf(parsed[i-1][0]) > -1){
           curve = new Bezier(cur[0], cur[1] , cur[0] + parsed[i][1] - parsed[i][3], cur[1] + parsed[i][2] - parsed[i][4], cur[0] + parsed[i][1], cur[1] + parsed[i][2] , cur[0] + parsed[i][3], cur[1] + parsed[i][4]);
@@ -67,27 +80,34 @@ export default function(svgString) {
         }
         length = length + curve.length();
         cur = [parsed[i][3] + cur[0], parsed[i][4] + cur[1]];
+        functions.push(curve);
       }
       //Quadratic Bezier curves
       else if(parsed[i][0] === "Q"){
+        console.info(cur[0], cur[1] , parsed[i][1], parsed[i][2] , parsed[i][3], parsed[i][4]);
         curve = new Bezier(cur[0], cur[1] , parsed[i][1], parsed[i][2] , parsed[i][3], parsed[i][4]);
         length = length + curve.length();
+        functions.push(curve);
         cur = [parsed[i][3], parsed[i][4]];
         prev_point = [parsed[i][1], parsed[i][2]];
+
       }  else if(parsed[i][0] === "q"){
         curve = new Bezier(cur[0], cur[1] , cur[0] + parsed[i][1], cur[1] + parsed[i][2] , cur[0] + parsed[i][3], cur[1] + parsed[i][4]);
         length = length + curve.length();
         prev_point = [cur[0] + parsed[i][1], cur[1] + parsed[i][2]];
         cur = [parsed[i][3] + cur[0], parsed[i][4] + cur[1]];
+        functions.push(curve);
       } else if(parsed[i][0] === "T"){
         if(i>0 && ["Q","q","T","t"].indexOf(parsed[i-1][0]) > -1){
           curve = new Bezier(cur[0], cur[1] , 2 * cur[0] - prev_point[0] , 2 * cur[1] - prev_point[1] , parsed[i][1], parsed[i][2]);
         } else {
           curve = new Bezier(cur[0], cur[1] , cur[0], cur[1] , parsed[i][1], parsed[i][2]);
         }
+        functions.push(curve);
         length = length + curve.length();
         prev_point = [2 * cur[0] - prev_point[0] , 2 * cur[1] - prev_point[1]];
         cur = [parsed[i][1], parsed[i][2]];
+
       } else if(parsed[i][0] === "t"){
         if(i>0 && ["Q","q","T","t"].indexOf(parsed[i-1][0]) > -1){
           curve = new Bezier(cur[0], cur[1] , 2 * cur[0] - prev_point[0] , 2 * cur[1] - prev_point[1] , cur[0] + parsed[i][1], cur[1] + parsed[i][2]);
@@ -97,6 +117,7 @@ export default function(svgString) {
         length = length + curve.length();
         prev_point = [2 * cur[0] - prev_point[0] , 2 * cur[1] - prev_point[1]];
         cur = [parsed[i][1] + cur[0], parsed[i][2] + cur[0]];
+        functions.push(curve);
       }
       partial_lengths.push(length);
 
@@ -108,9 +129,31 @@ export default function(svgString) {
     return length;
   };
 
-  svgProperties.getPointAt = function(){
-    return length;
+  svgProperties.getPointAt = function(fractionLength){
+    var i = partial_lengths.length - 1;
+
+    while(partial_lengths[i] >= fractionLength && partial_lengths[i] > 0){
+      i--;
+    }
+    i++;
+    var fractionPart = (fractionLength-partial_lengths[i-1])/partial_lengths[i];
+    console.info("--", fractionPart);
+    return functions[i].get(fractionPart);
   };
 
   return svgProperties(svgString);
+}
+
+
+function linearPosition(x0, x1, y0, y1){
+   function calculateLinearPosition(){
+      return linearPosition;
+    }
+
+    linearPosition.get = function(fraction){
+      var newDeltaX = (x1 - x0)*fraction;
+      var newDeltaY = (y1 - y0)*fraction;
+      return { x: x0 + newDeltaX, y: y0 + newDeltaY };
+    };
+  return calculateLinearPosition();
 }
---
+++ test/point-at-test.js
@@ -0,0 +1,94 @@
+var tape = require("tape"),
+path = require("../");
+require("./inDelta");
+
+var properties, xValues, yValues;
+
+tape("getPointAt testing lineTo", function(test) {
+
+  var paths = [{
+    path: "M0,50L500,50",
+    xValues: [ 0, 100, 200, 300, 400, 500 ],
+    yValues: [ 50, 50, 50, 50, 50, 50 ]
+  },{
+    path: "M0,50L300,300",
+    xValues: [ 0, 59.999996185302734, 119.99999237060547, 180, 239.99998474121094, 300 ],
+    yValues: [ 50, 100, 150, 200, 249.99998474121094, 300 ]
+  }];
+
+  for(var i=0; i< paths.length; i++){
+    for(var j=0; j<paths[i].xValues.length; j++){
+      properties = path.pathProperties(paths[i].path);
+      var position = properties.getPointAt(j*properties.getLength()/(paths[i].xValues.length-1));
+      test.inDelta(position.x, paths[i].xValues[j], 0.1);
+      test.inDelta(position.y, paths[i].yValues[j], 0.1);
+    }
+  }
+  test.end();
+
+});
+
+tape("getPointAt testing Cubic Bézier", function(test) {
+
+  var paths = [{
+    path: "M200,300 Q400,50 600,300",
+    xValues: [ 200, 267.53448486328125, 351.9363098144531, 448.0638732910156, 532.4654541015625, 600 ],
+    yValues: [ 300, 229.83480834960938, 182.21913146972656, 182.21926879882812, 229.83482360839844, 300 ]
+  }];
+
+  for(var i=0; i< paths.length; i++){
+    for(var j=0; j<paths[i].xValues.length; j++){
+      properties = path.pathProperties(paths[i].path);
+      console.info(j*properties.getLength()/(paths[i].xValues.length-1));
+      var position = properties.getPointAt(j*properties.getLength()/(paths[i].xValues.length-1));
+      test.inDelta(position.x, paths[i].xValues[j], 0.1);
+      test.inDelta(position.y, paths[i].yValues[j], 0.1);
+    }
+  }
+  test.end();
+
+  properties = path.pathProperties("M200,300 Q400,50 600,300");
+  console.info(1/5);
+  console.info(properties.getPointAt(0));
+  console.info(properties.getPointAt(0.20 * properties.getLength()));
+  console.info(properties.getPointAt(0.8 * properties.getLength()));
+
+});
+/*
+
+
+
+
+tape("Testing Cubic Bézier", function(test) {
+
+  //C & c
+  var properties = path.pathProperties("M0,100 Q50,-50 100,100 T200,100 T300,100");
+  console.info(properties.getPointAt(0.1 * properties.getLength()));
+  console.info("*****");
+  console.info(properties.getPointAt(0.5 * properties.getLength()));
+
+
+  //test.inDelta(properties.getLength(), 213.8, 0.1);
+
+
+  properties = path.pathProperties("m100,25c-90,65,10,75,50,170");
+  test.inDelta(properties.getLength(), 213.8, 0.1);
+
+
+  //S & s
+  properties = path.pathProperties("M100,200 C100,100 250,100 250,200 S400,300 400,200");
+  test.inDelta(properties.getLength(), 475.746, 0.1);
+
+  properties = path.pathProperties("M100,200 c0,-100 150,-100 150,0 s150,100 150,0");
+  test.inDelta(properties.getLength(), 475.746, 0.1);
+
+  //S & s without previous C or c
+  properties = path.pathProperties("M100,200 S400,300 400,200");
+  test.inDelta(properties.getLength(), 327.9618, 0.1);
+
+  properties = path.pathProperties("M100,200 s300,100 300,0");
+  test.inDelta(properties.getLength(), 327.9618, 0.1);
+
+  test.end();
+
+});*/
