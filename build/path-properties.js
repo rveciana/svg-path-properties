2c3219f116da2ff6bc516f9a85262c9d5d28758e
rveciana <rveciana@gmail.com>
2017-07-25 12:38:25 +0200

    new build

--- build/path-properties.js
+++ build/path-properties.js
@@ -1,4 +1,4 @@
-// http://geoexamples.com/path-properties/ Version 0.4.0. Copyright 2017 Roger Veciana i Rovira.
+// http://geoexamples.com/path-properties/ Version 0.4.1. Copyright 2017 Roger Veciana i Rovira.
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
 	typeof define === 'function' && define.amd ? define(['exports'], factory) :
