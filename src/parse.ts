92b7731dac2623644960eb1351883ad29514df65
Roger Veciana <rveciana@gmail.com>
2019-11-16 19:26:26 +0100

    Add the parse functions and tests

--- src/parse.ts
+++ src/parse.ts
@@ -1,6 +1,57 @@
-var length = { a: 7, c: 6, h: 1, l: 2, m: 2, q: 4, s: 4, t: 2, v: 1, z: 0 };
-var segment = /([astvzqmhlc])([^astvzqmhlc]*)/gi;
+type pathOrders = "a" | "c" | "h" | "l" | "m" | "q" | "s" | "t" | "v" | "z";
+const length: { [key in pathOrders]: number } = {
+  a: 7,
+  c: 6,
+  h: 1,
+  l: 2,
+  m: 2,
+  q: 4,
+  s: 4,
+  t: 2,
+  v: 1,
+  z: 0
+};
+const segmentRegExp = /([astvzqmhlc])([^astvzqmhlc]*)/gi;
+const numberRegExp = /-?[0-9]*\.?[0-9]+(?:e[-+]?\d+)?/gi;
 
 export default (path: string) => {
-  return "Hi";
+  const segments = path.match(segmentRegExp);
+  if (!segments) {
+    throw new Error(`No path elements found in string ${path}`);
+  }
+  return segments.reduce(
+    (segmentsArray: [string, ...Array<number>][], segmentString: string) => {
+      let command = segmentString.charAt(0);
+      let type: pathOrders = command.toLowerCase() as pathOrders;
+      const args = parseValues(segmentString.substr(1));
+
+      // overloaded moveTo
+      if (type === "m" && args.length > 2) {
+        segmentsArray.push([command, ...args.splice(0, 2)]);
+        type = "l";
+        command = command === "m" ? "l" : "L";
+      }
+
+      while (args.length >= 0) {
+        if (args.length === length[type]) {
+          segmentsArray.push([command, ...args.splice(0, length[type])]);
+          break;
+        }
+        if (args.length < length[type]) {
+          throw new Error(
+            `Malformed path data: "${command}" must have ${length[type]} elements and has ${args.length}: ${segmentString}`
+          );
+        }
+        segmentsArray.push([command, ...args.splice(0, length[type])]);
+      }
+
+      return segmentsArray;
+    },
+    []
+  );
+};
+
+const parseValues = (args: string) => {
+  var numbers = args.match(numberRegExp);
+  return numbers ? numbers.map(Number) : [];
 };
--- test/parse-test.ts
+++ test/parse-test.ts
@@ -1,8 +1,74 @@
 import * as test from "tape";
 import parse from "../src/parse";
 
-test("Parse", function(t) {
-  const res = parse("");
-  t.equal(res, "Hi");
-  t.end();
+test("overloaded moveTo", test => {
+  test.deepEqual(parse("m 12.5,52 39,0 0,-40 -39,0 z"), [
+    ["m", 12.5, 52],
+    ["l", 39, 0],
+    ["l", 0, -40],
+    ["l", -39, 0],
+    ["z"]
+  ]);
+
+  test.deepEqual(parse("M 12.5,52 39,0 0,-40 -39,0 z"), [
+    ["M", 12.5, 52],
+    ["L", 39, 0],
+    ["L", 0, -40],
+    ["L", -39, 0],
+    ["z"]
+  ]);
+
+  test.deepEqual(parse("z"), [["z"]]);
+  test.end();
+});
+
+test("curveTo", function(test) {
+  var a = parse("c 50,0 50,100 100,100 50,0 50,-100 100,-100");
+  var b = parse("c 50,0 50,100 100,100 c 50,0 50,-100 100,-100");
+
+  test.deepEqual(
+    a,
+    [
+      ["c", 50, 0, 50, 100, 100, 100],
+      ["c", 50, 0, 50, -100, 100, -100]
+    ],
+    "Correct parse c"
+  );
+  test.deepEqual(a, b, "Testing repeated");
+  test.end();
+});
+
+test("lineTo, h, v, l", function(test) {
+  test.throws(function() {
+    parse("l 10 10 0");
+  }, /Malformed/);
+  test.deepEqual(parse("l 10,10"), [["l", 10, 10]]);
+  test.deepEqual(parse("L 10,10"), [["L", 10, 10]]);
+  test.deepEqual(parse("l10 10 10 10"), [
+    ["l", 10, 10],
+    ["l", 10, 10]
+  ]);
+
+  test.deepEqual(parse("h 10.5"), [["h", 10.5]]);
+  test.deepEqual(parse("v 10.5"), [["v", 10.5]]);
+
+  test.end();
+});
+
+test("arcTo, quadratic curveTo, smooth curveTo, smooth quadratic curveTo", function(test) {
+  test.deepEqual(parse("A 30 50 0 0 1 162.55 162.45"), [
+    ["A", 30, 50, 0, 0, 1, 162.55, 162.45]
+  ]);
+
+  test.deepEqual(parse("M10 80 Q 95 10 180 80"), [
+    ["M", 10, 80],
+    ["Q", 95, 10, 180, 80]
+  ]);
+  test.deepEqual(parse("S 1 2, 3 4"), [["S", 1, 2, 3, 4]]);
+  test.deepEqual(parse("T 1 -2e2"), [["T", 1, -2e2]]);
+
+  test.throws(function() {
+    parse("t 1 2 3");
+  }, Error);
+  test.end();
 });
