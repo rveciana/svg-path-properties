8a023fbe0974c99f584be4839c5470634716a963
Roger Veciana <roger.vecianarovira@bp.com>
2020-10-02 10:24:48 +0200

    new version

--- dist/types/bezier-functions.d.ts
+++ dist/types/bezier-functions.d.ts
@@ -1,8 +1,5 @@
 import { Point } from "./types";
-export declare const cubicPoint: (xs: number[], ys: number[], t: number) => {
-    x: number;
-    y: number;
-};
+export declare const cubicPoint: (xs: number[], ys: number[], t: number) => Point;
 export declare const cubicDerivative: (xs: number[], ys: number[], t: number) => Point;
 export declare const getCubicArcLength: (xs: number[], ys: number[], t: number) => number;
 export declare const quadraticPoint: (xs: number[], ys: number[], t: number) => Point;
@@ -11,4 +8,4 @@ export declare const quadraticDerivative: (xs: number[], ys: number[], t: number
     x: number;
     y: number;
 };
-export declare const t2length: (length: number, total_length: number, func: (xs: number[], ys: number[], t: number) => number, xs: number[], ys: number[]) => number;
+export declare const t2length: (length: number, totalLength: number, func: (t: number) => number) => number;
